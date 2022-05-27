81e284bfc6eb1193834cd40d3792cc6eea37afbe
adambushman <adambushman@mail.weber.edu>
2022-05-26 19:17:04 -0600

    Additional plots

--- tree-map/practice_tree-map.R
+++ tree-map/practice_tree-map.R
@@ -17,8 +17,18 @@ data = data.frame (
 ) %>% mutate(inventory = ifelse(inventory < 0, 0, inventory))
 
 
+# Simple Tree Map
 
-ggplot(data %>% filter(item == 'TV'), aes(area = inventory
+ggplot(data, aes(area = inventory
+                 , label = paste(location, '\n', inventory, sep = '')
+                 , fill = group
+                 , subgroup = group)) +
+  geom_treemap()
+
+
+# Adding labels, titles, etc.
+
+ggplot(data, aes(area = inventory
                  , label = paste(location, '\n', inventory, sep = '')
                  , fill = group
                  , subgroup = group)) +
@@ -26,3 +36,50 @@ ggplot(data %>% filter(item == 'TV'), aes(area = inventory
   geom_treemap_text(colour = "white", place = "centre") +
   labs(title = 'Inventory Totals Across Retailers') +
   labs(subtitle = 'Product Category: TV')
+
+
+# Playing with different borders, groupings, and colors
+
+ggplot(data, aes(area = inventory
+                 , label = paste(location, '\n', inventory, sep = '')
+                 , fill = group
+                 , subgroup = group)) +
+  geom_treemap() +
+  scale_fill_manual(values = c('#987284', '#9DBF9E')) +
+  geom_treemap_text(colour = "white", place = "centre") +
+  geom_treemap_subgroup_border(colour = "black") +
+  labs(title = 'Inventory Totals Across Retailers') +
+  labs(subtitle = 'Product Category: TV')
+
+
+# New group, group labels, etc.
+
+ggplot(data, aes(area = inventory
+                 , label = paste(location, '\n', inventory, sep = '')
+                 , fill = group
+                 , subgroup = item)) +
+  geom_treemap() +
+  scale_fill_manual(values = c('#987284', '#9DBF9E')) +
+  geom_treemap_text(colour = "white", place = "centre") +
+  geom_treemap_subgroup_border(colour = "black") +
+  geom_treemap_subgroup_text(colour = "black", size = 10, place = "topleft", alpha = 0.6) +
+  labs(title = 'Inventory Totals Across Retailers') +
+  labs(subtitle = 'Product Category: TV')
+
+
+# Adding additional sub-group, changing label
+
+ggplot(data, aes(area = inventory
+                 , label = paste(prettyNum(inventory, big.mark = ","))
+                 , fill = group
+                 , subgroup = item
+                 , subgroup2 = location)) +
+  geom_treemap() +
+  scale_fill_manual(values = c('#987284', '#9DBF9E')) +
+  geom_treemap_text(colour = "white", place = "centre") +
+  geom_treemap_subgroup2_border(colour = "gray") +
+  geom_treemap_subgroup_border(colour = "black") +
+  geom_treemap_subgroup_text(colour = "black", size = 15, place = "topleft", alpha = 0.6) +
+  geom_treemap_subgroup2_text(colour = "black", size = 10, place = "bottomleft", alpha = 0.6) +
+  labs(title = 'Inventory Totals') +
+  labs(subtitle = 'By Category and Retailer')
