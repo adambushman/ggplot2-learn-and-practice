e7dc572a537a99e1c075dc0808497c863b080c6d
adambushman <adambushman@mail.weber.edu>
2022-09-07 12:49:04 -0600

    Updates to source file and travel transforms added

--- geographic-plots/example_nba-expansions.R
+++ geographic-plots/example_nba-expansions.R
@@ -17,7 +17,7 @@ library('ggrepel')
 data = read.csv('nba-expansion-data.csv')
 
 # Register using your own key (below is fake)
-register_google(key = 'aIzaSyCFd4FGbI7axkMV4iS63rl-SvJGfT4m3F4')
+register_google(key = 'AIzaSyCFd4FGbI7axkMV4iS63rl-SvJGfT4m3F4')
 
 coords <- 
   data %>% 
@@ -52,8 +52,46 @@ plot_usmap(regions = c("states"), exclude = c("Alaska", "Hawaii")) +
            label = paste("Expansion Author\n", auth, sep = ''), 
            hjust = 0) +
   labs(title = "Prospective NBA Expansion", 
-       subtitle = "32 Teams | 8 Divisions", 
+       subtitle = "32 Teams | 4 Divisions", 
        color = "Divisions") +
   theme(legend.position = "right", 
         plot.title = element_text(size = 20, face = "bold", hjust = 0.5), 
         plot.subtitle = element_text(size = 13, face = "italic", hjust = 0.5))
+
+
+locationName = c()
+divisionName = c()
+totalMiles = c()
+
+for(d in unique(expansionDF$division)) {
+  teams = expansionDF %>% 
+    filter(author == "@adam_bushman" & division == d) %>%
+    select(location) %>%
+    unlist(., use.names = FALSE)
+  
+  for(t in teams) {
+    total = 0
+    
+    for(o in (teams[teams != t])) {
+      total = total + pracma::haversine(
+        rev(unlist(coords[coords$location == t, 2:3], use.names = FALSE)), 
+        rev(unlist(coords[coords$location == o, 2:3], use.names = FALSE))
+      ) * 0.621371192
+    }
+    
+    locationName[length(locationName)+1] = t
+    divisionName[length(divisionName)+1] = d
+    totalMiles[length(totalMiles)+1] = total / 8
+  }
+}
+
+# TRAVEL STUFF
+
+x <- data.frame(locationName, divisionName, totalMiles)
+
+x %>% 
+  group_by(divisionName) %>% 
+  summarise(avg = mean(totalMiles), range = max(totalMiles) - min(totalMiles)) %>%
+  arrange(desc(avg))
+
+x %>% arrange(desc(totalMiles))
