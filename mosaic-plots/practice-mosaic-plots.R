86f9fd249cd9f8588b087a16a485431720c977a4
adambushman <adambushman@mail.weber.edu>
2022-11-29 09:57:43 -0700

    Data transform pipeline

--- mosaic-plots/practice-mosaic-plots.R
+++ mosaic-plots/practice-mosaic-plots.R
@@ -61,4 +61,58 @@ data <-
 head(data)
 
 
+###
+# Data Transform
+###
+
+data_clean <-
+  data %>%
+  # Sum up sales and inventory by regions and store
+  group_by(regions, category) %>%
+  summarise(
+    tInventory = sum(inventory), 
+    .groups = "drop"
+  ) %>%
+  ungroup(.) %>%
+  # Save a copy for later
+  assign("grpd", ., envir = .GlobalEnv) %>%
+  # Setup variables for the X and Y limits per category
+  group_by(category) %>%
+  mutate(
+    high = cumsum(tInventory), 
+    low = high - tInventory
+  ) %>%
+  ungroup(.) %>%
+  group_by(regions) %>%
+  mutate(
+    xmax = max(high), 
+    xmin = max(low)
+  ) %>%
+  ungroup(.) %>%
+  inner_join(
+    ., 
+    (
+      grpd %>%
+        group_by(regions) %>%
+        mutate(
+          ymax_s = cumsum(tInventory), 
+          ymin_s = ymax_s - tInventory
+        ) %>%
+        ungroup(.)
+    )
+  ) %>%
+  # Setup y-axis label
+  mutate(
+    xmax_s = xmax / max(xmax, xmin), 
+    xmin_s = xmin / max(xmax, xmin)
+  ) %>%
+  group_by(regions) %>%
+  mutate(
+    ylab = ymax_s, 
+    ylab_s = 0, 
+    lab_s = ifelse(category == "Paper", regions, ""), 
+    xlab = ((xmax_s - xmin_s) / 2) + xmin_s
+  ) %>%
+  select(lab_s, category, tInventory, xmax_s, xmin_s, ymax_s, ymin_s, xlab, ylab, ylab_s)
+
 
