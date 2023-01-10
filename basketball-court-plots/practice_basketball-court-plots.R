55ec76087321375d44081ecb537a0efaacdb1b61
adambushman <adambushman@mail.weber.edu>
2023-01-09 21:32:04 -0700

    x/y transforms setup and first plot

--- basketball-court-plots/practice_basketball-court-plots.R
+++ basketball-court-plots/practice_basketball-court-plots.R
@@ -2,6 +2,7 @@ library('tidyverse')
 library('hoopR')
 library('sportyR')
 
+
 df_pbp <- hoopR::load_nba_pbp()
 colnames(df_pbp)
 
@@ -15,13 +16,34 @@ test <-
     .groups = 'drop'
   )
 
+jazz_list = list(
+  offensive_half_court = "#FDFBE7", 
+  defensive_half_court = "#FDFBE7", 
+  court_apron = "#050505", 
+  free_throw_circle_fill = "#FDFBE7", 
+  center_circle_fill = "#FDFBE7", 
+  center_circle_outline = "#050505", 
+  painted_area = c("#FDFBE7"), 
+  restricted_arc = "#050505",
+  free_throw_circle_dash = "#050505", 
+  lane_space_mark = "#050505", 
+  two_point_range = "#FDFBE7",
+  backboard = "#050505"
+)
 
 geom_basketball(
   league = "NBA", 
-  display_range = "offense"
+  rotation = 270, 
+  display_range = "offense", 
+  color_updates = jazz_list, 
+  x_trans = -42, 
+  y_trans = 25
 ) +
   geom_point(
     data = test, 
-    aes(coordinate_x, coordinate_y), 
-    alpha = 0.5
-  )
+    aes(coordinate_x, coorinate_y, color = freq), 
+    alpha = 0.7, 
+    show.legend = FALSE
+  ) +
+  scale_color_gradient(low = "blue", high = "red")
+  
